10ff10485da0e88cb1a65a8146b643996d1ffea4
Pedro Henrique Oliveira Bezerra <pedro.11hob@gmail.com>
2026-08-17 20:02:31 -0300

    Classe Ranking criada

---
+++ Ranking.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Ranking {
+    
+    private List<User> usuarios;
+
+    // Construtor vazio
+    public Ranking() {
+    }
+
+    // Construtor com todos os atributos
+    public Ranking(List<User> usuarios) {
+
+        this.usuarios = usuarios;
+        
+    }
+
+
