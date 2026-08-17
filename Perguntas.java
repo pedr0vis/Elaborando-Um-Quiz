4213ced1a4621ab01a05bb6a10b7b0100a62d657
Pedro Henrique Oliveira Bezerra <pedro.11hob@gmail.com>
2026-08-17 19:42:07 -0300

    Classe perguntas criadas

---
+++ Perguntas.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Perguntas {
+
+    private List<String> opcoes;
+    private String enunciado;
+    private int respostaCorreta;
+
+
+}
