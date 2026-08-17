b5595bbbfad95138e546b7e12d609f77f937779a
Pedro Henrique Oliveira Bezerra <pedro.11hob@gmail.com>
2026-08-17 19:37:08 -0300

    Getters e Setters adicionados

--- User.java
+++ User.java
@@ -19,4 +19,27 @@ public class User {
         this.pontuacao = 0; // valor padrão
     }
 
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    public int getPontuacao() {
+
+        return pontuacao;
+
+    }
+
+    public void setPontuacao(int pontuacao) {
+
+        this.pontuacao = pontuacao;
+
+    }
+
 }
